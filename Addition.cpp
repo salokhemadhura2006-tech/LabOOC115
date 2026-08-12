84e00557451eabb0fd73a3da6625da22bca985c7
salokhemadhura2006-tech <salokhemadhura2006@gmail.com>
2026-08-12 04:16:51 +0000

    first

---
+++ Addition.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+     cout<<"Enter first Number";
+     cin>>a;
+     cout<<"Enter Second Number";
+     cin>>b;
+     c=a+b;
+     cout<<"Addition of two numbers is:"<<c;
+     return 0;
+     
+
+}
