41cc7e1653f88ba6fc7d4b436ececa0e28cfd900
salokhemadhura2006-tech <salokhemadhura2006@gmail.com>
2026-09-09 04:05:05 +0000

    commit

---
+++ exp5.2.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class construct
+{
+    public:
+    float area;
+
+    construct(){
+        area=0;
+    }
+
+    construct(int a,int b)
+    {
+        area=a*b;
+    }
+    void display()
+    {
+        cout<<area<<endl;
+
+    }
+
+};
+int main()
+{
+    construct c;
+    construct c1(5,3);
+    c.display();
+    c1.display();
+    cout<<"Area is:"<<area;
+    return 0;
+
+}
